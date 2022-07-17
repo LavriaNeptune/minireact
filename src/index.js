91f60e470d6431672be4eb0f60693090c4053ae4
Lavria <fangsunedu@gmail.com>
2022-07-17 18:35:43 +0800

    渲染流程优化

--- src/index.js
+++ src/index.js
@@ -42,22 +42,100 @@ function createTextElement(text) {
   };
 }
 
+function createDom(fiber) {}
+
 function render(element, container) {
-  const dom =
-    element.type == 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type);
-  // 进一步考虑:要根据元素类型进行不同的处理,即文本节点和元素节点两种情况。
-  const isProperty = (key) => key !== 'children';
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => (dom[name] = element.props[name]));
-  // 将元素的属性分配每个节点...
-  element.props.children.forEach((child) => {
-    render(child, dom);
-  });
-  // element 的子节点也要通过遍历 render 函数来创建
-  container.appendChild(dom);
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+// function render(element, container) {
+//   const dom =
+//     element.type == 'TEXT_ELEMENT'
+//       ? document.createTextNode('')
+//       : document.createElement(element.type);
+//   // 进一步考虑:要根据元素类型进行不同的处理,即文本节点和元素节点两种情况。
+//   const isProperty = (key) => key !== 'children';
+//   Object.keys(element.props)
+//     .filter(isProperty)
+//     .forEach((name) => (dom[name] = element.props[name]));
+//   // 将元素的属性分配每个节点...
+//   element.props.children.forEach((child) => {
+//     render(child, dom);
+//   });
+//   // element 的子节点也要通过遍历 render 函数来创建
+//   container.appendChild(dom);
+// }
+
+// ↑ 上述该渲染函数有一个存在一个缺陷:一旦开始渲染,不能停止渲染进度,必须要完整地渲染完整个元素树。如果元素树很大的话,可能会长时间阻塞主线程。让我们编写一些方法将渲染工作拆分为多个小任务,以便可以不必要地长时间阻塞主线程。
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+  // requestIdleCallback 是一个实验性质的方法:其作用是在当前线程没有任务时,调用传入其的回调函数...
+}
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+    // fiber dom 的值如果为空 表示非初始化状态 需要将自身转换为 dom 节点; 如果不为空 则为根节点渲染情况,自带容器元素
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+    // 如果为空 -> 节点为 root 节点,什么也不做 如果不为空 将自身 DOM 元素插入到父节点中
+  }
+  const elements = fiber.props.children;
+  // 取出 fiber 的所有子节点进行操作
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    // 只要 element 数组中还有元素,就一直进行渲染
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    // 依照虚拟 DOM 的结构,创建一个新的 fiber 对象 -> 主要是多了 parent 和 dom 属性
+    if (index === 0) {
+      fiber.child = newFiber;
+      // 当 index 为零时 -> 将其作为 fiber 的子节点插入
+    } else {
+      prevSibling.sibling = newFiber;
+      // 当 index 不为零时 -> 将其作为其前一个兄弟节点的后续节点插入
+    }
+    prevSibling = newFiber;
+    // 初始情况:将第一个 fiber 对象作为 prevSibling
+    index++;
+    // 节点计数器 +1
+    if (fiber.child) {
+      return fiber.child;
+      // 如果 fiber 存在子节点,则返回子节点
+    }
+    let nextFiber = fiber;
+    // 这里对 filer 做了存在性检测 -> fiber 存在再执行后续操作
+    while (nextFiber) {
+      if (nextFiber.sibling) {
+        return nextFiber.sibling;
+        // 如果 fiber 存在兄弟节点,则返回兄弟节点
+      }
+      nextFiber = nextFiber.parent;
+      // 如果 fiber 不存在兄弟节点,则返回父节点
+    }
+  }
 }
 
 const myReact = {
